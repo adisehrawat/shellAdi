e6865de97db67eb6fa9a346e3f1c4e73d341cce3
adisehrawat <sehrawataditya22@gmail.com>
2025-02-27 23:11:25 +0530

    added navigation part

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 set -e
 
-cd "$(dirname "$0")"
-cargo build --release --target-dir=/tmp/rust-build
+(
+    cd "$(dirname "$0")"
+    cargo build --release --target-dir=/tmp/rust-build
+)
+
 
 # Open a new Git Bash window and run the program
 mintty -e bash -c "/tmp/rust-build/release/shellAdi.exe; exec bash"
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use clap::{Arg, ArgMatches, Command};
 use pathsearch::find_executable_in_path;
+use std::env;
 use std::io::{self, Write};
 use std::path::Path;
 use std::process::{self, Command as ProcessCommand};
 
 fn main() {
+
     let stdin = io::stdin();
     let mut stdout = io::stdout();
     let mut input = String::new();
@@ -32,8 +34,8 @@ fn main() {
         let matches = Command::new("myshell")
             .subcommand(
                 Command::new("exit")
-                .about("Exit the shell")
-                .arg(Arg::new("CODE").required(false))
+                    .about("Exit the shell")
+                    .arg(Arg::new("CODE").required(false)),
             )
             .subcommand(
                 Command::new("echo")
@@ -45,6 +47,12 @@ fn main() {
                     .about("Finds command type")
                     .arg(Arg::new("CMD").required(true)),
             )
+            .subcommand(
+                Command::new("cd")
+                .about("move the the directory")
+                .arg(Arg::new("CD").required(true)),
+            )
+            .subcommand(Command::new("pwd").about("Tells the current directory"))
             .try_get_matches_from(std::iter::once("myshell").chain(args.iter().copied()));
 
         match matches {
@@ -73,7 +81,7 @@ fn handle_command(matches: ArgMatches) {
         Some(("type", sub_matches)) => {
             if let Some(cmd) = sub_matches.get_one::<String>("CMD") {
                 match cmd.as_str() {
-                    "echo" | "exit" | "type" => println!("{} is a shell builtin", cmd),
+                    "echo" | "exit" | "type" | "pwd" => println!("{} is a shell builtin", cmd),
                     _ => {
                         if let Some(path) = find_executable_in_path(cmd) {
                             println!("{} is {}", cmd, path.to_str().unwrap());
@@ -84,9 +92,45 @@ fn handle_command(matches: ArgMatches) {
                 }
             }
         }
+
+        Some(("cd", sub_matches)) => {
+            if let Some(path) = sub_matches.get_one::<String>("CD") {
+                if let Err(_e) = change_directory(path) {
+                    eprintln!("cd: {path}: No such file or directory");
+                }
+            }
+        }
+
+        Some(("pwd", _)) => {
+            let current_dir = env::current_dir().unwrap();
+            println!("{}", current_dir.display());
+        }
         _ => {}
     }
 }
+
+fn change_directory(path: &str) -> io::Result<()> {
+    let new_path = if path == "~" {
+        dirs::home_dir().unwrap_or_else(|| env::current_dir().unwrap()) 
+    } else {
+        let input_path = Path::new(path);
+        if input_path.is_absolute() {
+            input_path.to_path_buf()
+        } else {
+            env::current_dir()?.join(input_path) 
+        }
+    };
+
+    let new_path = new_path.canonicalize()?;
+
+    if new_path.is_dir() {
+        env::set_current_dir(&new_path)?;
+    } else {
+        eprintln!("Error: '{}' is not a valid directory", new_path.display());
+    }
+    Ok(())
+}
+
 
 fn external_handle(input: &str) {
     let mut parts = input.split_whitespace();
